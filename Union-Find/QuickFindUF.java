8537dcea27b4115fac2d6e0b19c77f7a725146a0
Milind452 <mohapatra.milind@gmail.com>
2021-03-24 13:06:31 +0530

    Add QuickFind class

---
+++ Union-Find/QuickFindUF.java
@@ -0,0 +1,31 @@
+// Given a set of N nodes check if two nodes are connected. 
+// If not connected, then connect them by giving them same ids
+
+
+public class QuickFindUF {
+    private int[] id;
+
+    public QuickUnionFind(int N) {
+        // Set id of each object to itself
+        id = new int[N]
+        for(int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public Boolean isConnected(int p, int q) {
+        // Check if p is connected to q by checking their ids
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {
+        // Change all entries of id[p] to id[q]
+        int pid = id[p];
+        int qid = id[q];
+        for(int i = 0; i < id.length; i++) {
+            if(id[i] == pid) {
+                id[i] = qid;
+            }
+        }
+    }
+}
