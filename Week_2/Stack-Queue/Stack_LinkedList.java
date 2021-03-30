e46dd692258e0c10e7594793c66f2105fb45114f
Milind452 <mohapatra.milind@gmail.com>
2021-03-30 11:17:25 +0530

    Add skeleton for stack implementation using linked list

---
+++ Week_2/Stack-Queue/Stack_LinkedList.java
@@ -0,0 +1,25 @@
+public class Stack_LinkedList {
+
+    // Points to the top of the stack
+    private Node top = null;
+
+    // Node of the stack containing our data and a reference to the prev node
+    private class Node {
+
+    }
+
+    // Checks is the stack is empty or not
+    public boolean isEmpty() {
+
+    }
+
+    // Insert new node into the stack
+    public void push(String item) {
+
+    }
+
+    // Remove and return the top most node
+    public String pop() {
+
+    }
+}
