7c9386e60b8ccf34c56d10b25bf82af8d6988e59
Milind452 <mohapatra.milind@gmail.com>
2021-03-31 17:19:01 +0530

    Add skeleton for queue implementation using linked list

---
+++ Week_2/Stack-Queue/Queue_LinkedList.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Queue_LinkedList {
+
+    // Points to the first node in the queue
+    private Node first;
+
+    // Points to the last node in the queue
+    private Node last;
+
+    // Node of the queue containing our data and a reference to the prev node
+    private class Node {
+        String item;
+        Node next;
+    }
+
+    // Checks if the queue is empty or not
+    public Boolean isEmpty() {
+        return false;
+    }
+
+    // Insert new node to the end of the queue
+    public void enqueue(String item) {
+
+    }
+
+    // Remove and return the first node in the queue
+    public String dequeue() {
+        return null;
+    }
+}
