cbe2d81b3d63d0ce4eeb055b2b91c179e55eff87
Milind452 <mohapatra.milind@gmail.com>
2021-04-01 16:10:33 +0530

    Remove redundant imports

--- Week_2/Stack-Queue/Deque-and-randomized-queues/Deque.java
+++ Week_2/Stack-Queue/Deque-and-randomized-queues/Deque.java
@@ -2,7 +2,6 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Iterator;
-import java.util.ListIterator;
 
 public class Deque<Item> implements Iterable<Item> {
     
