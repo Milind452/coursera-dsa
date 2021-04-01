80510d8c2fefe223db07a5240cbc8894e27c9236
Milind452 <mohapatra.milind@gmail.com>
2021-04-01 15:01:31 +0530

    Implement isEmpty function

--- Week_2/Stack-Queue/Deque-and-randomized-queues/Deque.java
+++ Week_2/Stack-Queue/Deque-and-randomized-queues/Deque.java
@@ -24,7 +24,7 @@ public class Deque<Item> implements Iterable<Item> {
 
     // is the deque empty?
     public boolean isEmpty() {
-        
+        return first == null;
     }
 
     // return the number of items on the deque
