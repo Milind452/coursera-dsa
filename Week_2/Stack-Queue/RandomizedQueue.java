2a083c14f1645f9aff06a1d4980c30c38b4a5ebe
Milind452 <mohapatra.milind@gmail.com>
2021-04-05 12:47:18 +0530

    Implement is empty function

--- Week_2/Stack-Queue/RandomizedQueue.java
+++ Week_2/Stack-Queue/RandomizedQueue.java
@@ -23,7 +23,9 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     // is the randomized queue empty?
-    public boolean isEmpty();
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
     // return the number of items on the randomized queue
     public int size();
