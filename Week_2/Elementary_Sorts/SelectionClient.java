71dd5d1f3a9027c37bab7b6af1f3378fcfaf12cf
Milind452 <mohapatra.milind@gmail.com>
2021-04-06 12:37:09 +0530

    Implement selection sort client

---
+++ Week_2/Elementary_Sorts/SelectionClient.java
@@ -0,0 +1,26 @@
+public class SelectionClient implements Comparable<SelectionClient>{
+
+    private int x;
+
+    public int compareTo(SelectionClient that) {
+        if (this.x < that.x) {
+            return -1;
+        } else if(this.x > that.x) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Comparable[] a = {33, 44, 77, 11, 66, 99, 22, 55};
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ",");
+        }
+        System.out.println();
+        Selection.sort(a);
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ",");
+        }
+    }
+}
